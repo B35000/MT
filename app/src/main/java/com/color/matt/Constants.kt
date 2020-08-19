6296dc5c6c9fd4f290028a81a217106d0be271bc
bryonyoni <bryonyoni@gmail.com>
2020-08-19 10:50:12 +0300

    set up project

---
+++ app/src/main/java/com/color/matt/Constants.kt
@@ -0,0 +1,5 @@
+package com.color.matt
+
+class Constants {
+
+}
---
+++ app/src/main/java/com/color/matt/Utilities/ApiStuff.kt
@@ -0,0 +1,6 @@
+package com.color.matt.Utilities
+
+class ApiStuff {
+    /*the api key for places and geocoder service in gcloud. DO NOT MAKE THESE PUBLIC AND CHANGE PERIODICALLY. The file is in the .gitignore(Project level) btw.*/
+    val geocoder_api_key = "AIzaSyCHtqBJiDiVCpekcSaHq6GpnsHJxX_y8fo"
+}
