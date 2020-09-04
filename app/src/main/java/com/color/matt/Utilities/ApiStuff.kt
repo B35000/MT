a874bf141bd68ab4cc4c062563393a8b422cad3a
bryonyoni <bryonyoni@gmail.com>
2020-09-04 19:02:49 +0300

    removed old apis class

--- app/src/main/java/com/color/matt/Utilities/ApiStuff.kt
+++
@@ -1,6 +0,0 @@
-package com.color.matt.Utilities
-
-class ApiStuff {
-    /*the api key for places and geocoder service in gcloud. DO NOT MAKE THESE PUBLIC AND CHANGE PERIODICALLY. The file is in the .gitignore(Project level) btw.*/
-    val geocoder_api_key = "AIzaSyCHtqBJiDiVCpekcSaHq6GpnsHJxX_y8fo"
-}
